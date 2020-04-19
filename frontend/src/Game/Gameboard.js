e6e63668303662f04d00e76d886d8e02da1f945c
scrabill <shannon.crabill@gmail.com>
2020-04-19 14:58:37 -0400

    Adds state constructor object

--- frontend/src/Game/Gameboard.js
+++ frontend/src/Game/Gameboard.js
@@ -5,20 +5,30 @@ import PlayerInput from './PlayerInput'
 
 export default class Gameboard extends React.Component {
 
+    constructor(props) {
+      super(props);
+      this.state = {
+        currentTime: 30,
+        currentScore: 0,
+        currentEmoji: "🍔",
+        activeGame: true
+      }
+    }
+
     render() {
       return (
         <div>
-          <h3>Time Remaining: {this.props.currentTime}</h3>
-          <h3>Score: {this.props.currentScore}</h3>
-          <Emoji currentEmoji={"🍔"}/>
+          <h3>Time Remaining: {this.state.currentTime}</h3>
+          <h3>Score: {this.state.currentScore}</h3>
+          <Emoji currentEmoji={this.state.currentEmoji}/>
 
           <br />
 
           <Button text="Start Game"/>
-          <Button text="Food"/>
+          <Button text="Food" activeGame={this.state.activeGame}/>
           <Button text="Foe"/>
 
-          <PlayerInput />
+          <PlayerInput activeGame={this.state.activeGame} />
 
         </div>
       );
