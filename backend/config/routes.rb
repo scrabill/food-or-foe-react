1c63dd002ff674ab4957621fc3e8d083c2cf2b10
scrabill <shannon.crabill@gmail.com>
2020-04-20 17:51:13 -0400

    Adds routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      get '/emojis/food',  to: 'emojis#food'
+      get '/games/leaderboard',  to: 'games#leaderboard'
+
+      resources :emojis, :users, :games
+    end
+  end
 end
