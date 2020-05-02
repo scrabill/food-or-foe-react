a0bca505ba23e05bcd3898a9ffabd72ff88ff3ba
scrabill <shannon.crabill@gmail.com>
2020-05-02 15:55:37 -0400

    Update fetchScores to pull from leaderboard endpoint to show scores in order of nighest to lowest

--- frontend/src/Actions/scoreActions.js
+++ frontend/src/Actions/scoreActions.js
@@ -2,7 +2,7 @@ export const fetchScores = () => {
   return (dispatch) => {
     dispatch({ type: 'LOADING_SCORES' })
 
-    fetch('http://localhost:3000/api/v1/games')
+    fetch('http://localhost:3000/api/v1/games/leaderboard')
     .then(r => {
       return r.json()
     })
