d6cb84479ee006e3736d53bae66aaaa7f8055d4a
scrabill <shannon.crabill@gmail.com>
2020-05-03 17:34:31 -0400

    Adds link to personal website in footer

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <footer>
-      <em>Food or Foe?</em> created by Shannon Crabill
+      <em>Food or Foe?</em> created by <a href="http://shannoncrabill.com/">Shannon Crabill</a>
     </footer>
   )
 }
