816aac1c4e2bc3a1b7ba9288465bdce80db8694e
scrabill <shannon.crabill@gmail.com>
2020-05-03 17:34:20 -0400

    Updates About page

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -4,7 +4,14 @@ export const About = () => {
   return (
     <div>
       <h2>About</h2>
-      <p>This game is rad and I think you should play it.</p>
+
+      <p>Food or Foe? Was built with a Rails API backend and React frontend.</p>
+
+      <p>State is managed with Redux with Thunk as middleware.</p>
+
+      <p>This game was brought to life as a technical project for the Flatiron School Software Engineering Bootcamp</p>
+
+      <p>The project repo can be found here: <a href="https://github.com/scrabill/food-or-foe-react" >https://github.com/scrabill/food-or-foe-react</a></p>
     </div>
   );
 }
