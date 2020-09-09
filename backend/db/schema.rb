4e1966ddc57102ddc479a8a4db66320adbe10f12
scrabill <shannon.crabill@gmail.com>
2020-09-09 19:43:16 -0400

    Adds schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_20_213616) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "emojis", force: :cascade do |t|
     t.string "slug"
     t.string "character"
