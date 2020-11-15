4dd1f03b4bc1c9294842eb72a1c9fc8d4ff55dcf
scrabill <shannon.crabill@gmail.com>
2020-11-15 15:31:52 -0500

    initial demo commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,48 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     let emoji = document.querySelector("emoji")
-    window.setInterval(displayEmoji, 1500);
+    let buttons = document.querySelectorAll("button")
+
+    displayEmoji()
+
+    console.log(buttons)
+
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            makeAGuess(event)
+        })
+    });
 
 });
 
 const emojis = ["🍔","🍝","🧀","🍤","🌭","🚀","🦄","💾","💎","💡"]
+let score = document.querySelector("#score")
+
+function makeAGuess(event) {
+    let guess = event.target.id
+    let currentEmoji = emoji.innerHTML
+    let foodEmojis = ["🍔","🍝","🧀","🍤","🌭"]
+    let foeEmojis = ["🚀","🦄","💾","💎","💡"]
+
+    if (guess == "food") {
+        updateScore(foodEmojis.includes(currentEmoji))
+    } else {
+        updateScore(foeEmojis.includes(currentEmoji))
+    }
+
+}
+
+function updateScore(result) {
+
+
+
+    if (result == true) {
+        score.innerHTML++
+    } else {
+        score.innerHTML--
+    }
+
+    displayEmoji()
+}
 
 function getRandomEmoji() {
     return (
